7da64d312293ce6b5f66fd25aa0a3d9fb4cff99a
stonejing <stonejing1817@gmail.com>
2024-12-11 08:57:14 +0900

    do not develop in conoha

--- app/(game)/2048/page.tsx
+++ app/(game)/2048/page.tsx
@@ -1,9 +1,8 @@
 
-
 export default function Page() {
   return (
     <div className="min-h-screen flex items-center justify-center">
-      <h1>Home</h1>
+      <h1>2048</h1>
     </div>
   );
 }
--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,9 +1,8 @@
 
-
 export default function Page() {
   return (
     <div className="min-h-screen flex items-center justify-center">
-      <h1>Home</h1>
+      <h1>About</h1>
     </div>
   );
 }
